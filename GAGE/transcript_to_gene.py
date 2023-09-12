0e52056cbc689e7dbe4e82ec089f92c97fd2290f
felipevzps <felipe.vzps@gmail.com>
2023-09-12 14:33:36 -0300

    fixed script to translate gene names

--- GAGE/transcript_to_gene.py
+++ GAGE/transcript_to_gene.py
@@ -1,19 +1,19 @@
-transcript_to_gene = {}
+translate_gene_name = {}
 
-with open('Chlamydomonas_reinhardtii.Chlamydomonas_reinhardtii_v5.5.55.gff3', 'r') as gff_file:
+with open('GAGE/Chlamydomonas_reinhardtii.Chlamydomonas_reinhardtii_v5.5.55.gff3', 'r') as gff_file:
     for line in gff_file:
         if not line.startswith('#'):
             fields = line.strip().split('\t')
-            if len(fields) >= 9 and fields[2] == 'mRNA':
+            if len(fields) >= 9 and fields[2] == 'gene':
                 attributes = fields[8].split(';')
-                transcript_id = gene_id = None
+                gene_name = gene_id = None
                 for attr in attributes:
                     key, value = attr.strip().split('=')
-                    if key == 'ID':
-                        transcript_id = value
-                    elif key == 'Parent':
-                        gene_id = value
-                if transcript_id and gene_id:
-                    transcript_to_gene[transcript_id] = gene_id
+                    if key == 'ID' and value.startswith('gene:'):
+                        gene_id = value[5:] # Remove 'gene:' prefix, single quotes, and extra whitespace
+                    elif key == 'Name':
+                        gene_name = value # Remove single quotes and extra whitespace
+                if gene_name and gene_id:
+                    translate_gene_name[gene_id] = gene_name
 
-print(transcript_to_gene)
+print(translate_gene_name)
