3818843b30c3cc4653678a09c8480367f8610156
felipevzps <felipe.vzps@gmail.com>
2023-09-27 17:28:08 -0300

    added NOISeq_48h/

---
+++ GO_enrichment/NOISeq_48h/GO_enrichment_48h.R
@@ -0,0 +1,54 @@
+# topGO Chlamydomonas reinhardtii 48h
+
+rm(list=ls())
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("topGO")
+
+library(topGO)
+
+# Notebook
+#setwd("/home/felipe/Documents/DESEQ_Chlamydomonas_reinhardtii/GO_enrichment/NOISeq_48h")
+
+# CENA
+setwd("/home/felipevzps/Documentos/DESEQ_Chlamydomonas_reinhardtii/GO_enrichment/NOISeq_48h")
+
+dados <- read.table("DEGs_NOISeqTech_SALT48H.mod.mod.csv", header = FALSE, sep = ",")
+dados
+
+go_annotations <- readMappings("GO_annotations.txt", sep = "\t", IDsep=",")
+go_annotations
+
+genes_ranking <- dados[, c(1, ncol(dados))]
+genes_ranking
+
+genes_ranking[, 2] <- as.numeric(genes_ranking[, 2])
+genes_ranking[, 2]
+
+allGenes <- setNames(genes_ranking[, 2], genes_ranking[, 1])
+allGenes
+
+# Função de seleção de genes simples (selecione todos os genes como significativos)
+geneSelectionFun <- function(allGenes) {
+  return(rep(TRUE, length(allGenes)))
+}
+
+# Crie o objeto topGOdata com o vetor numérico
+topGOdata <- new("topGOdata", ontology = "BP", allGenes = allGenes,
+                 annot = annFUN.gene2GO, gene2GO = go_annotations,
+                 geneSel = geneSelectionFun)
+
+fishers_result <- runTest(topGOdata, algorithm = "classic", statistic = "fisher")
+#			 -- Classic Algorithm -- 
+#the algorithm is scoring 836 nontrivial nodes
+#parameters: 
+#  test statistic: fisher
+
+allRes <- GenTable(topGOdata, classic = fishers_result, ranksOf = "classic", topNodes=836)
+allRes
+
+topGO_all_table <- allRes[order(allRes$classic),]
+topGO_all_table
+
+write.table(topGO_all_table, file = paste0("topGO_Chlamydomonas_reinhardtii_48h", ".BP", ".csv"), sep =",", quote= F, col.names = T)
