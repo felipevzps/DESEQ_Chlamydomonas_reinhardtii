c1cc64fb9a18242d5144a58bf3f0a41dd25c2d6c
felipevzps <felipe.vzps@gmail.com>
2023-09-28 12:10:58 -0300

    updated code to extract gene ID from enriched GO terms

--- GO_enrichment/NOISeq_24h/GO_enrichment_24h.R
+++ GO_enrichment/NOISeq_24h/GO_enrichment_24h.R
@@ -9,10 +9,10 @@ BiocManager::install("topGO")
 library(topGO)
 
 # Notebook
-#setwd("/home/felipe/Documents/DESEQ_Chlamydomonas_reinhardtii/GO_enrichment/NOISeq_24h")
+setwd("/home/felipe/Documents/DESEQ_Chlamydomonas_reinhardtii/GO_enrichment/NOISeq_24h")
 
 # CENA
-setwd("/home/felipevzps/Documentos/DESEQ_Chlamydomonas_reinhardtii/GO_enrichment/NOISeq_24h")
+#setwd("/home/felipevzps/Documentos/DESEQ_Chlamydomonas_reinhardtii/GO_enrichment/NOISeq_24h")
 
 dados <- read.table("DEGs_NOISeqTech_SALT24H.mod.mod.csv", header = FALSE, sep = ",")
 dados
@@ -39,6 +39,10 @@ topGOdata <- new("topGOdata", ontology = "BP", allGenes = allGenes,
                  annot = annFUN.gene2GO, gene2GO = go_annotations,
                  geneSel = geneSelectionFun)
 
+# Create a map of geneIDs to GO terms
+ann.genes <- genesInTerm(topGOdata)
+str(ann.genes)
+
 fishers_result <- runTest(topGOdata, algorithm = "classic", statistic = "fisher")
 #			 -- Classic Algorithm -- 
 #the algorithm is scoring 1496 nontrivial nodes
@@ -52,3 +56,18 @@ topGO_all_table <- allRes[order(allRes$classic),]
 topGO_all_table
 
 write.table(topGO_all_table, file = paste0("topGO_Chlamydomonas_reinhardtii_24h", ".BP", ".csv"), sep =",", quote= F, col.names = T)
+
+# Create a map of geneIDs/GO terms on the GO terms from the Fisher analysis
+fisher.go <- names(score(fishers_result))
+fisher.ann.genes <- genesInTerm(topGOdata, whichGO = fisher.go)
+fisher.ann.genes
+
+# Use lapply para formatar os elementos da lista como linhas de texto
+formatted_lines <- lapply(names(fisher.ann.genes), function(go_id) {
+  genes <- fisher.ann.genes[[go_id]]
+  return(paste(go_id, paste(genes, collapse = ", "), sep = "\t"))
+})
+formatted_lines
+
+# Escreva todas as linhas formatadas em um arquivo de texto
+write.table(formatted_lines, file = paste0("topGO_Chlamydomonas_reinhardtii_24h_ann.genes.BP.csv"), quote= F, col.names = F, row.names = F, sep="\n")
